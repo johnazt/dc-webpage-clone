e84e61e17dda83c6f4375259fb55f162e805fdff
John Asto <john.azt.dev@gmail.com>
2023-01-30 17:59:31 -0300

    add section video

--- src/components/SectionVideo.jsx
+++ src/components/SectionVideo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SectionVideo = () => (
-	<section className='flex flex-col m-auto lg:max-w-[960px] lg:flex-row xl:max-w-[1140px] xxl:max-w-[1320px] mb-20 px-3  h-full max-w-[540px] md:max-w-[720px] gap-4 '>
+	<section className='flex flex-col m-auto lg:max-w-[960px] lg:flex-row xl:max-w-[1140px] xxl:max-w-[1320px] mb-20 px-3  h-full max-w-[540px] md:max-w-[720px] xl:gap-7 '>
 		<iframe
 			className='w-full min-h-[200px] sm:min-h-[310px] xl:w-[57%] xl:min-h-[430px]'
 			src='https://www.youtube.com/embed/Q__dO3pEYDU?rel=0'
@@ -12,7 +12,7 @@ const SectionVideo = () => (
 			<p className='uppercase font-extrabold tracking-tighter my-4'>
 				official Trailer 2
 			</p>
-			<p className='text-[1.35rem] md:text-[2rem] uppercase font-extrabold  mb-3 tracking-tighter'>
+			<p className='text-[1.35rem] md:text-[2.2rem] uppercase font-extrabold  mb-3 tracking-tighter'>
 				Shazam! fury of the gods
 			</p>
 			<small className='text-[14px]'>
