8442ad9eb7ac70968e25ce18586d817d1a1c215c
John Asto <john.azt.dev@gmail.com>
2023-02-04 11:32:12 -0300

    add design slider

--- src/components/LatestCard.jsx
+++ src/components/LatestCard.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export const LatestCard = ({ card }) => {
-	return <div>{card.category}</div>;
+	return (
+		<div className='flex flex-col  text-center shadow-md basis-0 flex-1 min-w-[200px] lg:min-w-[280px] pb-5'>
+			<img
+				className='min-h-[100px] lg:min-h-[150px] object-fill basis-0 flex-grow-1'
+				src={card.img}
+				alt={card.title}
+			/>
+
+			<div className='basis-0 flex-grow-1 '>
+				<small className='tracking-widest text-gray-600 my-4 inline-block'>
+					{card.category}
+				</small>
+				<p className='uppercase font-extrabold  px-2 tracking-tighter overflow-hidden'>
+					{card.title}
+				</p>
+			</div>
+		</div>
+	);
 };
--- src/components/LatestTrailers.jsx
+++ src/components/LatestTrailers.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import { latest } from '../constants';
 import { LatestCard } from './LatestCard';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 export const LatestTrailers = () => {
-	console.log(latest);
 	return (
-		<div
-			className=' mb-4 m-auto px-3 sm:px-0
-        sm:max-w-[436px]
-         md:max-w-[616px]
-         lg:max-w-[856px]
-         xl:max-w-[1036px]
-         xxl:max-w-[1216px]
-         '
-		>
-			<h2 className='uppercase text-[1.35rem] font-extrabold tracking-tighter md:text-[2.2rem]'>
-				latest trailers, clips & more
-			</h2>
-
-			<div>
-				{latest.map(card => (
-					<LatestCard card={card} />
-				))}
+		<div className=' mb-4 m-auto px-3 sm:px-0 sm:max-w-[436px] md:max-w-[616px] lg:max-w-[960px] xl:max-w-[1036px] xxl:max-w-[1216px] relative'>
+			<div className='relative  sm:mx-4 md:mx-8 lg:mx-2 sm:overflow-hidden '>
+				<h2 className='uppercase text-[1.35rem] font-extrabold tracking-tighter md:text-[2.2rem]'>
+					latest trailers, clips & more
+				</h2>
+				<ul className='carousel flex gap-6 pb-6 mt-8 text-sm'>
+					{latest.map(card => (
+						<LatestCard card={card} key={card.title} />
+					))}
+				</ul>
+			</div>
+			<div className='absolute bg-white rounded-full p-3 flex cursor-pointer drop-shadow-xl top-[50%] opacity-0 md:opacity-95 lg:-left-4'>
+				<ArrowBackIosIcon color='primary' className='pl-1' />
+			</div>
+			<div className='absolute bg-white rounded-full p-3 flex cursor-pointer drop-shadow-xl top-[50%] opacity-0 md:opacity-95 lg:-right-4'>
+				<ArrowForwardIosIcon color='primary' className='pl-1' />
 			</div>
 		</div>
 	);
