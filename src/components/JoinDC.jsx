80e466ae11d8a4b8160f868f3375d3240438533c
John Asto <john.azt.dev@gmail.com>
2023-02-11 08:08:17 -0300

    add joindc section

--- src/components/JoinDC.jsx
+++ src/components/JoinDC.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export const JoinDC = () => {
 	return (
-		<div className='flex flex-col text-white bg-gradient-to-r from-[#2a0541] to-[#5816bd] lg:flex-row px-11 pt-6'>
-			<div>
-				<p className='uppercase text-[1.35rem] pb-4 font-extrabold tracking-tighter md:text-[2.2rem] scale-y-110 md:text-5xl'>
+		<div className='flex flex-col text-white bg-gradient-to-r from-[#2a0541] to-[#5816bd] px-11 pt-6 lg:flex-row lg:items-center'>
+			<div className='lg:w-[60%]'>
+				<p className='uppercase text-[1.35rem] pb-4 font-extrabold tracking-tighter md:text-5xl scale-y-110 '>
 					JOIN THE DC UNIVERSE
 				</p>
 				<p className='text-sm leading-6 font-semibold opacity-90 mb-4'>
@@ -17,9 +17,9 @@ export const JoinDC = () => {
 				</button>
 			</div>
 			<img
-				className='f'
+				className='lg:w-[40%]'
 				src='https://static.dc.com/2022-06/DC_Peacemaker_RegBan_cutout.png?w=700'
-				alt=''
+				alt='img-footer'
 			/>
 		</div>
 	);
