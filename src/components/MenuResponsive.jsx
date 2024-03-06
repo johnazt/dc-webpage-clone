5efec6bf97738b077bc12917de335a2863492ca9
John Asto <john.azt.dev@gmail.com>
2024-03-06 13:59:06 -0300

    add fixes to ul list

--- src/components/MenuResponsive.jsx
+++ src/components/MenuResponsive.jsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import React from 'react'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
 
 export const MenuResponsive = ({ category, menuResponsive }) => {
-	return (
-		<div
-			className={
+  return (
+    <div
+      className={
 				menuResponsive
 					? `h-full fixed bottom-0 left-0 right-0 top-12 translate-y-0 opacity-100 -z-40 transition bg-white duration-300`
 					: `h-full fixed  duration-100 -translate-y-[110%] -z-40 opacity-0 left-0 right-0`
 			}
 		>
-			<div className=' h-full  border-t-gray-400 border-2'>
-				<ul className='flex flex-col gap-8 font-bold uppercase text-[14px]  tracking-[-1.5px]  border-b-black border-[1px] p-4 '>
-					{category.map(item => (
-						<span
-							key={item.category}
-							className='cursor-pointer hover:text-blue-500 inline-block'
+      <div className=' h-full  border-t-gray-400 border-2'>
+        <ul className='flex flex-col gap-8 font-bold uppercase text-[14px]  tracking-[-1.5px]  border-b-black border-[1px] p-4 '>
+          {category.map(item =>
+            <li
+              key={item.category}
+              className='cursor-pointer hover:text-blue-500 inline-block'
 						>
-							{item.category}
-							{item.options.length > 0 && <ArrowDropDownIcon />}
-						</span>
-					))}
-				</ul>
-				<div className='flex p-8 justify-center gap-4 font-bold '>
-					<button className='w-full border-black rounded-full px-20 py-3 border-[1px] tracking-[-1.5px] text-[14px] hover:bg-blue-500 hover:text-white hover:border-transparent transition'>
+              {item.category}
+              {item.options.length > 0 && <ArrowDropDownIcon />}
+            </li>
+					)}
+        </ul>
+        <div className='flex p-8 justify-center gap-4 font-bold '>
+          <button className='w-full border-black rounded-full px-20 py-3 border-[1px] tracking-[-1.5px] text-[14px] hover:bg-blue-500 hover:text-white hover:border-transparent transition'>
 						SIGN UP
 					</button>
-					<button className='w-full border-black rounded-full px-20 py-3 border-[1px] tracking-[-1.5px] text-[14px] hover:bg-blue-500 hover:text-white hover:border-transparent transition'>
+          <button className='w-full border-black rounded-full px-20 py-3 border-[1px] tracking-[-1.5px] text-[14px] hover:bg-blue-500 hover:text-white hover:border-transparent transition'>
 						LOG IN
 					</button>
-				</div>
-			</div>
-		</div>
-	);
-};
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/screens/SectionVideo.jsx
+++ src/screens/SectionVideo.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from 'react'
 
-export const SectionVideo = () => (
-	<section className='flex flex-col m-auto lg:max-w-[960px] lg:flex-row xl:max-w-[1140px] xxl:max-w-[1320px] mb-16 px-3  h-full max-w-[540px] md:max-w-[720px] xl:gap-7 '>
-		<iframe
-			className='w-full min-h-[200px] sm:min-h-[310px] xl:w-[57%] xl:min-h-[430px]'
-			src='https://www.youtube.com/embed/Q__dO3pEYDU?rel=0'
-			allow='accelerometer; encrypted-media; gyroscope; picture-in-picture'
-			allowFullScreen
-		></iframe>
-		<div className='lg:self-center'>
-			<p className='uppercase font-extrabold tracking-tighter my-4'>
+export const SectionVideo = () =>
+  <section className='flex flex-col m-auto lg:max-w-[960px] lg:flex-row xl:max-w-[1140px] xxl:max-w-[1320px] mb-16 px-3  h-full max-w-[540px] md:max-w-[720px] xl:gap-7 '>
+    <iframe
+      className='w-full min-h-[200px] sm:min-h-[310px] xl:w-[57%] xl:min-h-[430px]'
+      src='https://www.youtube.com/embed/Q__dO3pEYDU?rel=0'
+      allow='accelerometer; encrypted-media; gyroscope; picture-in-picture'
+      title='Trailer de próxima pelicula en estreno'
+      allowFullScreen
+		/>
+    <div className='lg:self-center'>
+      <p className='uppercase font-extrabold tracking-tighter my-4'>
 				official Trailer 2
 			</p>
-			<p className='text-[1.35rem] md:text-[2.2rem] uppercase font-extrabold  mb-3 tracking-tighter scale-y-110'>
+      <p className='text-[1.35rem] md:text-[2.2rem] uppercase font-extrabold  mb-3 tracking-tighter scale-y-110'>
 				Shazam! fury of the gods
 			</p>
-			<small className='text-[14px]'>
+      <small className='text-[14px]'>
 				It's Time to Grow Up.<i> Shazam! Fury of the Gods</i> - in the theaters
 				March 17.
 			</small>
-		</div>
-	</section>
-);
+    </div>
+  </section>
